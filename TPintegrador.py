62a18499342acd38bc970c7c8945ece59fb2eae1
mateocaranta <mateocaranta@gmail.com>
2023-10-18 14:53:30 -0300

    Validacion de credenciales y arreglo menu

--- TPintegrador.py
+++ TPintegrador.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from contrasenia import generar_contrasenia
+
+lista_alumnos = []  
+lista_profesores = []
 
 class Usuario(ABC):
     def __init__(self, nombre, apellido, email, contrasenia):
@@ -28,31 +32,101 @@ class Estudiante(Usuario):
         self.cursos.append(curso)
         print(f"Estudiante {self.nombre} matriculado en el curso {curso.nombre}")
 
+    def validar_credenciales(self, email, contrasenia):
+        super().validar_credenciales(email, contrasenia)
+        if email == self.email and contrasenia == self.contrasenia:  
+            return True
+        else:
+            return False
 
-def menu_principal (self):
-    print ("---Menu---")
+
+class Profesor(Usuario):
+    def __init__(self, nombre, apellido, email, contrasenia, titulo, anio_egreso):
+        super().__init__(nombre, apellido, email, contrasenia)
+        self.titulo = titulo
+        self.anio_egreso = anio_egreso
+        self.cursos = []
+
+    def str(self):
+        return super().str() + f" \n- Título: {self.titulo}"
+
+    def dictar_curso(self, curso):
+        self.cursos.append(curso)
+        print(f"Profesor {self.nombre} dictando el curso {curso.nombre}")
+
+    def validar_credenciales(self, email, contrasenia):
+        super().validar_credenciales(email, contrasenia)
+        if email == self.email and contrasenia == self.contrasenia:
+            return True
+        else:
+            return False
+
+
+class Curso: 
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.contrasenia_matriculacion = generar_contrasenia()
+
+    
+    def str(self):
+        return f"Curso: {self.nombre}"
+
+
+def menu_principal ():
+    print ("\n---Menu---")
     print ("1- Ingresar como alumno.")
     print ("2- Ingresar como profesor.")
     print ("3- Ver cursos.")
     print ("4- Salir.")  
-    opcion = int(input())
+    print ("")
+    opcion = int(input("Seleccione una opcion:\n"))
     return opcion
 
-def programa_principal ():
-    opcion = 0 
+def programa_principal():
+    opcion = 0
+
     while opcion != 4:
         opcion = menu_principal()
-
         if opcion == 1:
+            validacion_email = input("Ingrese su email:\n")
             print("")
+            validacion_contrasenia = input("Ingrese su contrasenia:\n")
+            alumno_encontrado = None
+            for alumno in lista_alumnos:
+                if alumno.validar_credenciales(validacion_email, validacion_contrasenia):
+                    alumno_encontrado = alumno
+                    break
+            if alumno_encontrado:
+                print(f"Bienvenido, {alumno_encontrado.nombre}")
+            else:
+                print("Credenciales incorrectas o estudiante inexistente, debe darse de alta en alumnado.")
+        
         elif opcion == 2:
+            validacion_email_profe = input("Ingrese su email:\n")
             print("")
+            validacion_contrasenia_profe = input("Ingrese su contrasenia:\n")
+            profe_encontrado = None
+            for profe in lista_profesores:
+                if profe.validar_credenciales(validacion_email_profe, validacion_contrasenia_profe):
+                    profe_encontrado = profe
+                    break
+            if profe_encontrado:
+                print(f"Bienvenido, {profe_encontrado.nombre}")
+            else:
+                print("Credenciales incorrectas o profe inexistente, debe darse de alta en alumnado.")
         elif opcion == 3:
-            print("")
+            print("Mostrar cursos aquí.")
         elif opcion == 4:
             print("Hasta luego!!")
             break
         else:
             print("Ingrese una opcion correcta! (1-4)")
-        
-        
+
+
+alumno = Estudiante("Mauro", "Brizio", "mauro@gmail.com", "123", 123, 2023)
+lista_alumnos.append(alumno)
+
+profesor = Profesor("Mateo", "Caranta", "mateo@gmail.com", "123", "Licenciado", 1990)
+lista_profesores.append(profesor)
+
+programa_principal()
